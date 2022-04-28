ca9216a697b4ff52969ba417768d131012647335
Harrison Burt <hburt2003@gmail.com>
2022-04-28 14:22:36 +0100

    Add cors

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
+const origins = CORS_ORIGINS.split(",");
+
 /**
  * Respond with worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
+  let url = new URL(request.url);
+  const resp = await fetch(
+      `${API_URL}/link/${url.pathname}`,
+      {
+        method: "GET",
+        headers: {
+          "X-API-KEY": API_KEY
+        }
+      }
+  );
 
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  let cors = {};
+  if (origin.includes(request.headers.referrer)) {
+    cors["Access-Control-Allow-Origin"] = request.headers.referrer;
+    cors["Access-Control-Allow-Methods"] = "GET, OPTIONS";
+  }
+
+
+  return new Response(resp.body, {
+    headers: {
+      ...resp.headers,
+      ...cors,
+    }
   })
 }
 
